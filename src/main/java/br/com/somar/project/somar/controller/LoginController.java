d803665eeca714b14cd1265ca326c191edf8ab90
etmamate <maateus.cristo132@gmail.com>
2025-09-17 02:29:51 -0300

    implementando novos controllers para os endpoints de "Login" e "Registro"

--- src/main/java/br/com/somar/project/somar/controller/LoginController.java
+++ src/main/java/br/com/somar/project/somar/controller/LoginController.java
@@ -1,10 +1,19 @@
 package br.com.somar.project.somar.controller;
 
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.somar.project.somar.dto.LoginRequestDTO;
+import br.com.somar.project.somar.dto.RegisterRequestDTO;
+import br.com.somar.project.somar.dto.ResponseDTO;
 import br.com.somar.project.somar.infra.security.TokenService;
+import br.com.somar.project.somar.model.Usuario;
 import br.com.somar.project.somar.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -16,4 +25,33 @@ public class LoginController {
     private final PasswordEncoder passwordEncoder;
     private final TokenService tokenService;
 
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequestDTO body) {
+        Usuario usuario = this.usuarioRepository.findByEmail(body.email())
+                .orElseThrow(() -> new RuntimeException("Usuario não encontrado"));
+        if (passwordEncoder.matches(usuario.getSenha(), body.senha())) {
+            String token = this.tokenService.generateToken(usuario);
+            return ResponseEntity.ok(new ResponseDTO(usuario.getNome(), token));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterRequestDTO body) {
+        Optional<Usuario> usuario = this.usuarioRepository.findByEmail(body.email());
+
+        if (usuario.isEmpty()) {
+            Usuario newUsuario = new Usuario();
+            newUsuario.setSenha(passwordEncoder.encode(body.senha()));
+            newUsuario.setEmail(body.email());
+            newUsuario.setNome(body.nome());
+            this.usuarioRepository.save(newUsuario);
+
+            String token = this.tokenService.generateToken(newUsuario);
+            return ResponseEntity.ok(new ResponseDTO(newUsuario.getNome(), token));
+
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
 }
---
+++ src/main/java/br/com/somar/project/somar/dto/ResponseDTO.java
@@ -0,0 +1,5 @@
+package br.com.somar.project.somar.dto;
+
+public record ResponseDTO(String name, String nome) {
+    
+}
