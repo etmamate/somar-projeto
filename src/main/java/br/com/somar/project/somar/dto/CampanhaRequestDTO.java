277f5cf9a8c214c911122b69e48762658de46b4e
Mateus Cristo <34775078+etmamate@users.noreply.github.com>
2025-11-13 10:48:51 -0300

    fet: implementando método POST para cadastro de campanha

--- src/main/java/br/com/somar/project/somar/dto/CampanhaRequestDTO.java
+++ src/main/java/br/com/somar/project/somar/dto/CampanhaRequestDTO.java
@@ -1,5 +1,6 @@
 package br.com.somar.project.somar.dto;
 
-public record CampanhaResquestDTO(String titulo, String descricao, int meta, String localizacao) {
+public record CampanhaRequestDTO(String titulo, String descricao, int meta, String localizacao) {
 
 }
+;
---
+++ src/main/java/br/com/somar/project/somar/repository/CampanhaRepository.java
@@ -0,0 +1,10 @@
+package br.com.somar.project.somar.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.somar.project.somar.model.Campanha;
+
+public interface CampanhaRepository extends JpaRepository<Campanha, Long>{
+
+    
+} 
